7754f2ef865d178e2c4a29961fe3f36bb4435d0f
toshi <toshi@pixiv.com>
2013-12-09 18:20:11 +0900

    build.sbt -> Build.scala

---
+++ project/Build.scala
@@ -0,0 +1,46 @@
+import sbt._
+import Keys._
+import com.typesafe.sbt.SbtStartScript
+
+object AlpacaBuild extends Build {
+
+  lazy val root = Project(
+    id = "root",
+    base = file(".")
+  ).aggregate(lang)
+
+  lazy val lang = Project (
+    id = "lang",
+    base = file ("lang"),
+    settings = Defaults.defaultSettings ++ Seq (
+      name := "alpaca-lang",
+      organization := "com.github.tototoshi",
+      version := "0.1.0-SNAPSHOT",
+      scalaVersion := "2.10.2",
+      libraryDependencies ++= Seq(
+        "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+      ),
+      resolvers += Resolver.sonatypeRepo("public"),
+      libraryDependencies ++= Seq(
+      "org.seleniumhq.selenium" % "selenium-java" % "2.37.1",
+      "com.github.scopt" %% "scopt" % "3.2.0",
+      "com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
+      "ch.qos.logback" % "logback-classic" % "1.0.13",
+      "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+      ),
+      scalacOptions ++= Seq("-deprecation", "-language:_")
+    ) ++ SbtStartScript.startScriptForClassesSettings
+  )
+
+  lazy val test = Project (
+    id = "test",
+    base = file ("test"),
+    settings = Defaults.defaultSettings ++ Seq (
+      name := "alpaca-lang-test",
+      organization := "com.github.tototoshi",
+      version := "0.1.0-SNAPSHOT",
+      scalaVersion := "2.10.2"
+    )
+  )
+
+}
